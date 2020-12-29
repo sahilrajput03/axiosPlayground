adbe77cb74cfb8b22da180a5eca4916f5d5b93cb
Sahil Rajput <sahilrajput03@gmail.com>
2020-12-29 16:37:31 +0530

    Simple predeploy method. ~sahil

---
+++ docs/build/precache-manifest.923fa6fa4eb003281240650c8bbac300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a9fada666eb71c444e8c683319ccf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06094aff177474c4863c",
+    "url": "/static/css/main.a0866368.chunk.css"
+  },
+  {
+    "revision": "8ee0a50f1037ce1acb5a",
+    "url": "/static/js/2.e5761e8f.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.e5761e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06094aff177474c4863c",
+    "url": "/static/js/main.7542496c.chunk.js"
+  },
+  {
+    "revision": "8040032296ce5d6531f8",
+    "url": "/static/js/runtime-main.5a322ded.js"
+  }
+]);
